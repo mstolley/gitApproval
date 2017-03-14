f04b4de3b2dbf93b62627b552cc62a84f93b5021
Matthew Stolley <matthew.stolley@ge.com>
2017-03-14 14:14:34 -0500

    [add_options] Update js

--- options.js
+++ options.js
@@ -1,32 +1,27 @@
-// Saves options to chrome.storage.sync.
-// function save_options() {
-//   var color = document.getElementById('color').value;
-//   var likesColor = document.getElementById('like').checked;
-//   chrome.storage.sync.set({
-//     favoriteColor: color,
-//     likesColor: likesColor
-//   }, function() {
-//     // Update status to let user know options were saved.
-//     var status = document.getElementById('status');
-//     status.textContent = 'Options saved.';
-//     setTimeout(function() {
-//       status.textContent = '';
-//     }, 750);
-//   });
-// }
-//
-// // Restores select box and checkbox state using the preferences
-// // stored in chrome.storage.
-// function restore_options() {
-//   // Use default value color = 'red' and likesColor = true.
-//   chrome.storage.sync.get({
-//     favoriteColor: 'red',
-//     likesColor: true
-//   }, function(items) {
-//     document.getElementById('color').value = items.favoriteColor;
-//     document.getElementById('like').checked = items.likesColor;
-//   });
-// }
-// document.addEventListener('DOMContentLoaded', restore_options);
-// document.getElementById('save').addEventListener('click',
-//     save_options);
+function save_options() {
+    var host = document.getElementById('host').value;
+    var approvalNumber = document.getElementById('approvalNumber').value;
+    chrome.storage.sync.set({
+        host: host,
+        approvalNumber: approvalNumber
+    }, function() {
+        // Update status to let user know options were saved.
+        var status = document.getElementById('status');
+        status.textContent = 'Options saved.';
+        setTimeout(function() {
+            status.textContent = '';
+        }, 750);
+    });
+}
+
+function restore_options() {
+    chrome.storage.sync.get({
+        host: 'https://github.com',
+        approvalNumber: '2'
+    }, function(items) {
+        document.getElementById('host').value = items.host;
+        document.getElementById('approvalNumber').value = items.approvalNumber;
+    });
+}
+document.addEventListener('DOMContentLoaded', restore_options);
+document.getElementById('save').addEventListener('click', save_options);
